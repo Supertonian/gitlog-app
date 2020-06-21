0599f79fc3ea973fdf851b28ef1cddad1ee1d5fa
Changyun Lee <brownbearpower@gmail.com>
2020-06-22 03:47:25 +0900

    load fr0m json

---
+++ src/GitLog.js
@@ -0,0 +1,40 @@
+const gitlog = require("gitlog").default;
+
+const options = {
+  repo: __dirname,
+  number: 20,
+//   author: "Changyun Lee",
+  fields: ["hash", "abbrevHash", "subject", "authorName", "authorDateRel"],
+  execOptions: { maxBuffer: 1000 * 1024 },
+};
+
+const commits = gitlog(options);
+
+var fs = require('fs');
+fs.writeFile("src/test.json", JSON.stringify(commits), function(err) {
+    if (err) {
+        console.log(err);
+    }
+});
+
+// export default function GitLog() {
+//     // Synchronous
+//     return (
+//         <Container>
+//             {commits}
+//         </Container>
+//         );
+// }
+
+// Asynchronous (with Callback)
+// gitlog(options, function (error, commits) {
+  // Commits is an array of commits in the repo
+//   console.log(commits);
+// });
+
+// const { gitlogPromise } = require("gitlog");
+
+// Asynchronous (with Promise)
+// gitlogPromise(options)
+//   .then((commits) => console.log(commits))
+//   .catch((err) => console.log(err));
--- src/SimpleTable.js
+++ src/SimpleTable.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -7,54 +6,52 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import data from './test.json';
 
-const useStyles = makeStyles({
-  table: {
-    minWidth: 650,
-  },
-});
+export default class SimpleTable extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      rows: [],
+    };
+  }
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
+  componentDidMount() {
+    this.setState({
+      rows: data,
+    });
+  }
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
-export default function SimpleTable() {
-  const classes = useStyles();
-
-  return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Git commit 메시지</TableCell>
-            <TableCell align="right">작성자</TableCell>
-            <TableCell align="right">날짜</TableCell>
-            <TableCell align="right">Hash</TableCell>
-            <TableCell align="right">팀명</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row, index) => (
-            <TableRow key={index}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+  render() {
+    return (
+      <TableContainer component={Paper}>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Git commit 메시지</TableCell>
+              <TableCell align="right">작성자</TableCell>
+              <TableCell align="right">날짜</TableCell>
+              <TableCell align="right">Hash</TableCell>
+              <TableCell align="right">팀명</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
+          </TableHead>
+          <TableBody>
+         {
+          this.state.rows.map((row, index) => (
+          <TableRow key={index}>
+            <TableCell component="th" scope="row">
+              {row.subject}
+            </TableCell>
+            <TableCell align="right">{row.authorName}</TableCell>
+            <TableCell align="right">{row.authorDateRel}</TableCell>
+            <TableCell align="right">{row.abbrevHash}</TableCell>
+            <TableCell align="right">{row.abbrevHash}</TableCell>
+          </TableRow>
+        ))
+      }
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+  }
 }
