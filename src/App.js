a6d497bc12caf53f43bf423c1296cbf5c6ccf50b
Changyun Lee <brownbearpower@gmail.com>
2020-06-21 22:15:22 +0900

    remove unnecessary parts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+function Buttons({team}) {
+  return <button onClick={()=>console.log(team)}>{ team }</button>;
+}
+
+class TeamSelector extends React.Component {
+  state = {
+    team: 'calendar'
+  }
+  buttonClick() {
+    this.setState({team: 'gg'});
+  }
+  render() {
+    return <div>
+      <h1>hello {this.state.team}</h1>
+      <Buttons team='calendar' />
+      <Buttons team='talk' />
+      <Buttons team='drive' />
+      </div>;
+  }
+}
+
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <TeamSelector />
       </header>
     </div>
   );
