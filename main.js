ee0337495b879908b79ace9f5db326c9a4918ca4
Strength <brownbearpower@gmail.com>
2020-09-23 22:43:27 +0900

    fix: 가로, 세로 크기 수정

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const path = require('path');
 function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 1100,
-    height: 1000,
+    width: 800,
+    height: 900,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true,
